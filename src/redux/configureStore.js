4b7814036b75ff639ba445e69acf3d2201f3e9ff
Karanpal Singh <singhkaranpal81@gmail.com>
2018-12-24 17:28:17 +0530

    React Redux Form Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,7 +6,7 @@ import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createForms } from 'react-redux-form';
-import { InitialFeedback } from './forms';
+import { InitialFeedback } from './form';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -18,6 +18,12 @@ export const ConfigureStore = () => {
             ...createForms({
                 feedback: InitialFeedback
             })
+
+            //So as soon as I use the initial feedback, visuals will record automatically imported that from forms. So, once we import the 
+            // initial feedback and so if we initialize it like this, this we'll add in the necessary reducer functions and also the state 
+            // information into my create store. So as you recall I mentioned that the React-Redux-Form brings in its own set of support for 
+            // all these on our behalf, so we don't need to write our own reducers or our action creators and so on, React-Redux-Form fills in
+            //  all the details by itself.
         }),
         applyMiddleware(thunk,logger)
     );
